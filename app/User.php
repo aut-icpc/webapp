ac6b42fbd3ee0cda001b50fc7925e5c45ddd8a49
Amir Haghighati <amirh2008@gmail.com>
2016-09-07 22:48:21 +0430

    cards commented on index page

--- app/User.php
+++ app/User.php
@@ -10,7 +10,6 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-//use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
  * Class User
