85de12f4534ea9ce7032c14ddb25cb14d6a28470
Amir Haghighati <amirh2008@gmail.com>
2016-09-22 10:37:57 +0330

    navbar updated for admin.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,23 +1,23 @@
-@if(Auth::check())
-    <ul id="auth_dropdown" class="dropdown-content">
-        <li>
-            <a href="{{ route('app::admin') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                {{ Auth::user()->name }} <span class="caret"></span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ url('/logout') }}"
-               onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                Logout
-            </a>
+{{--@if(Auth::check())--}}
+    {{--<ul id="auth_dropdown" class="dropdown-content">--}}
+        {{--<li>--}}
+            {{--<a href="{{ route('app::admin') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
+                {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
+            {{--</a>--}}
+        {{--</li>--}}
+        {{--<li>--}}
+            {{--<a href="{{ url('/logout') }}"--}}
+               {{--onclick="event.preventDefault();--}}
+                                                 {{--document.getElementById('logout-form').submit();">--}}
+                {{--Logout--}}
+            {{--</a>--}}
 
-            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-            </form>
-        </li>
-    </ul>
-@endif
+            {{--<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">--}}
+                {{--{{ csrf_field() }}--}}
+            {{--</form>--}}
+        {{--</li>--}}
+    {{--</ul>--}}
+{{--@endif--}}
 <header class="site-header">
     <div class="navbar-fixed">
         <nav class="cyan darken-2 waves-effect waves-light">
@@ -28,40 +28,34 @@
                     </a>
                     <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
+                        <li>
+                            <a href="{{ route('app::local.history') }}">History</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('app::general.about') }}">About</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('app::live.feed') }}">Live Blog!</a>
+                        </li>
                         @if(Auth::check())
                             <li>
-                                <a class="dropdown-button" href="#" data-activates="auth_dropdown">Login/Register
-                                    <i class="material-icons right">arrow_drop_down</i>
-                                </a>
-                            </li>
-                        @else
-                            <li>
-                                <a href="{{ route('app::local.history') }}">History</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('app::general.about') }}">About</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('app::live.feed') }}">Live Blog!</a>
+                                <a href="{{ route('app::admin') }}">Administration</a>
                             </li>
                         @endif
                     </ul>
                     <ul class="side-nav" id="mobile-demo">
+                        <li>
+                            <a href="{{ route('app::local.history') }}">History</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('app::general.about') }}">About</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('app::live.feed') }}">Live Blog!</a>
+                        </li>
                         @if(Auth::check())
                             <li>
-                                <a class="dropdown-button" href="#" data-activates="auth_dropdown">Login/Register
-                                    <i class="material-icons right">arrow_drop_down</i>
-                                </a>
-                            </li>
-                        @else
-                            <li>
-                                <a href="{{ url('/register') }}">Live Blog!</a>
-                            </li>
-                            <li>
-                                <a href="{{ url('/register') }}">Live Blog!</a>
-                            </li>
-                            <li>
-                                <a href="{{ url('/register') }}">Live Blog!</a>
+                                <a href="{{ route('app::admin') }}">Administration</a>
                             </li>
                         @endif
                     </ul>
@@ -70,12 +64,3 @@
         </nav>
     </div>
 </header>
-
-@push('scripts')
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $(".dropdown-button").dropdown();
-            $(".button-collapse").sideNav();
-        });
-    </script>
-@endpush
