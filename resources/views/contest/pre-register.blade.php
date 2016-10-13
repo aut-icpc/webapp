5cae9de9ad7596e780bf4658a49d9fbb6cabaf1a
Amir Haghighati <amirh2008@gmail.com>
2016-10-13 13:40:11 +0330

    pre register page updated.

--- resources/views/contest/pre-register.blade.php
+++ resources/views/contest/pre-register.blade.php
@@ -15,6 +15,14 @@
                 Note: If you see your registered team status is 'Pending' for more than 24 hours check your mail.
                 If you have not recieved any mail from group let us know via
                 <a href="mailto:ceit.ssc@aut.ac.ir">ceit.ssc@aut.ac.ir</a> <br>
+                <center>
+                    <a href="{{ route('app::contest.registered') }}">
+                        <button class="darken-2 waves-effect waves-light btn-large cyan" type="button" name="action">Check Registration Status
+                            <i class="material-icons right">toc</i>
+                        </button>
+                    </a>
+                </center>
+                <br>
             </p>
             <br>
 
@@ -27,12 +35,20 @@
 
                 Participation fees for AUT teams would be 900,000 Rials, and for non-AUT teams 12,000,000 Rials. These
                 fees only apply to Iranian contestants and payment information will be sent to teams via their email.<br>
+                <center>
+                    <a href="{{ route('app::contest.registered') }}">
+                        <button class="darken-2 waves-effect waves-light btn-large cyan" type="button" name="action">Register for On-Site Contest
+                            <i class="material-icons right">supervisor_account</i>
+                        </button>
+                    </a>
+                </center>
+                <br>
             </p>
             <br>
 
             <h5>New Registration For On-Line Contest</h5>
             <p>
-                To participate in the on-line contest, your coach has to register your team at the Baylor site.
+                To participate in the on-line contest, your coach has to register your team via <a href="{{ route('app::contest.register.online') }}">this form</a>.
 
                 Internet contest has no participation fees.
 
