f3a892d709e13d54c2e2df42db5908a477390fac
Amir Haghighati <amirh2008@gmail.com>
2016-10-13 13:52:31 +0330

    404 error blade added.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('parallax')
+    <div class="parallax-container">
+        <div class="parallax">
+            <img src="{{ asset('img/icpc.png') }}">
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    <div id="non-skrollr">
+        <div class="container-fluid section">
+            <div class="mast">
+                <img class="responsive-img" style="width: 60%;" src="{{ asset('img/aut_icpc.png') }}">
+                <h3>Whoooooooops...!</h3>
+                <h4>Something went wrong!</h4>
+                <p>
+                    Your Requested URI doesn't exist!
+                </p>
+                <br>
+                <a href="{{ route('app::index') }}">
+                    <button class="darken-2 waves-effect waves-light btn-large cyan" type="button" name="action">Get Back
+                    </button>
+                </a>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('ul.tabs').tabs();
+        $('.parallax').parallax();
+    });
+</script>
+@endpush
