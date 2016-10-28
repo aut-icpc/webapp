30248316b1499dfbbf1fb4bf7f5379bb07dea4b3
Amir Haghighati <amirh2008@gmail.com>
2016-10-28 18:21:52 +0330

    admin online registrations updated.

--- resources/views/admin/online_registrations.blade.php
+++ resources/views/admin/online_registrations.blade.php
@@ -8,6 +8,30 @@
             <p>
                 AUT ACM ICPC 2016 Online Contest - Registered Teams.
             </p>
+            <div class="row section">
+                <center>
+                    <h4>
+                        TODO: Review these registrations:
+                    </h4>
+                </center>
+                <ul class="collection">
+                    @foreach($data as $team)
+                        @if(!$team->register_is_ok)
+                            <li class="collection-item avatar">
+                                <i class="material-icons circle">textsms</i>
+                                <span class="title">{{ $team->team_name }}</span>
+                                <p>
+                                    Institution: {{ $team->institute }}<br>
+                                    Country: {{ $team->site }}
+                                </p>
+                                <a href="{{ route('app::admin.registration.edit', $team) }}" class="secondary-content">
+                                    <i class="material-icons">mode_edit</i>
+                                </a>
+                            </li>
+                        @endif
+                    @endforeach
+                </ul>
+            </div>
             <table class="highlight bordered centered">
                 <thead>
                 <tr>
