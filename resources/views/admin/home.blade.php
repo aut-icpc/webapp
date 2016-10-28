102c7d9c7a5a9f4a4e08d118c0aba692a846c4fa
Amir Haghighati <amirh2008@gmail.com>
2016-10-28 16:35:17 +0330

    Administration panel wave view updated.

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -15,7 +15,7 @@
         <div class="row">
             <div class="col s12 m12 l4">
                 <a href="{{ route('app::admin.live') }}">
-                    <div class="card cyan darken-2 waves-effect waves-teal">
+                    <div class="card cyan darken-2 waves-effect waves-light">
                         <div class="card-content white-text">
                             <span class="card-title">Live Blogging</span>
                             <p>View, Edit and publish posts on live blog.</p>
@@ -28,7 +28,7 @@
             <div class="row">
                 <div class="col s12 m12 l4">
                     <a href="{{ route('app::admin.registrations.show') }}">
-                        <div class="card cyan darken-2 waves-effect waves-teal">
+                        <div class="card cyan darken-2 waves-effect waves-light">
                             <div class="card-content white-text">
                                 <span class="card-title">On-Site Registrations</span>
                                 <p>View and edit On-Site Registrations and their team information.</p>
@@ -38,7 +38,7 @@
                 </div>
                 <div class="col s4 m4 l4">
                     <a href="{{ route('app::admin.online_registrations.show') }}">
-                        <div class="card cyan darken-2 waves-effect waves-teal">
+                        <div class="card cyan darken-2 waves-effect waves-light">
                             <div class="card-content white-text">
                                 <span class="card-title">On-Line Registrations</span>
                                 <p>View and edit On-Line Registrations and their team information.</p>
@@ -50,7 +50,7 @@
             <div class="row">
                 <div class="col s4 m4 l4">
                     <a href="{{ route('app::admin.mail.form') }}">
-                        <div class="card cyan darken-2 waves-effect waves-teal">
+                        <div class="card cyan darken-2 waves-effect waves-light">
                             <div class="card-content white-text">
                                 <span class="card-title">Email Form</span>
                                 <p>Send Email to contestants (using ceit.ssc94@gmail.com)</p>
