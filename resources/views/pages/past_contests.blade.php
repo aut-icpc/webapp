aa369c3cb9f198b13d413603000bbf7c658504e8
Amir Haghighati <amirh2008@gmail.com>
2016-11-19 13:48:26 +0330

    OnLine contest final rank list added.

--- resources/views/pages/past_contests.blade.php
+++ resources/views/pages/past_contests.blade.php
@@ -26,7 +26,7 @@
                 <br>
                 2014: <a href="http://ceit.aut.ac.ir/acm/files/past/14/Local/problem-set.pdf">Problems</a>,  <a href="http://ceit.aut.ac.ir/acm/files/past/14/Local/Local.htm">Final Ranklist (Onsite)</a>, <a href="http://ceit.aut.ac.ir/acm/files/past/14/Local/Online.htm">Final Ranklist (Online)</a> Test Data
                 <br>
-                2016: <a href="http://icpc.aut.ac.ir/files/aut2016.pdf">Problems</a>,  <a href="http://icpc.aut.ac.ir/files/onsite_ranking2016.html">Final Ranklist (Onsite)</a>
+                2016: <a href="http://icpc.aut.ac.ir/files/aut2016.pdf">Problems</a>,  <a href="http://icpc.aut.ac.ir/files/onsite_ranking2016.html">Final Ranklist (Onsite)</a>, <a href="https://www.hackerrank.com/contests/aut-icpc2016-online-version/leaderboard">Final Ranklist (Online)</a>
             </p>
             <div class="divider"></div>
 
