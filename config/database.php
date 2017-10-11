ee0d3473a03ceed43a602bb8ac4814db50d4547f
Parham Alvani <parham.alvani@gmail.com>
2017-10-11 16:25:30 -0400

    Corrects Mologuent error.

--- config/database.php
+++ config/database.php
@@ -86,6 +86,7 @@ return [
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
+            'name'    => 'mongodb'
             'options' => [
                 'database' => 'admin' // sets the authentication database required by mongo 3
             ]
