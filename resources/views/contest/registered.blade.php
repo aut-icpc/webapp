b36551a55e1ccb0b84d679aa4880a0d725c7cebc
kimiarm <kimia.rezaei96@gmail.com>
2017-10-22 17:26:29 +0330

    Update registered.blade.php

--- resources/views/contest/registered.blade.php
+++ resources/views/contest/registered.blade.php
@@ -14,8 +14,8 @@
                  if your team name was not in registerd team list 24 hours after regitration ,  please inform us by :
 
  <br>
-                                Email: <a style="color: #ffeb3b;" href="mailto:ceit.ssc94@gmail.com">ceit.ssc94@gmail.com</a> <br>
-                                Telegram : <a style="color: #ffeb3b;" href="https://t.me/ceitssc">https://t.me/ceitssc</a> <br>
+                                Email: <a style="color:  #00b0ff;" href="mailto:ceit.ssc94@gmail.com">ceit.ssc94@gmail.com</a> <br>
+                                Telegram : <a style="color:  #00b0ff;" href="https://t.me/ceitssc">https://t.me/ceitssc</a> <br>
                                
                             
             </p>
