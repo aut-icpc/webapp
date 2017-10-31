a7e0dd857cc0c836b01578a81af5b62a976eec10
kimiarm <kimia.rezaei96@gmail.com>
2017-10-31 09:56:50 +0330

    Update ContestController.php

--- app/Http/Controllers/ContestController.php
+++ app/Http/Controllers/ContestController.php
@@ -52,7 +52,7 @@ class ContestController extends Controller
 
     public function showContestRegisteredTeams() {
         $data = OnsiteRegistration::all()->sortBy('created_at');
-        LOG::info(OnsiteRegistration::all()) ; 
+        LOG::info(OnsiteRegistration::all()->toArray()) ; 
         return view('contest.registered', ['data' => $data]);
     }
 
