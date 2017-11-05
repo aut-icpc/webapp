a84700372cb53f48141cf1ad425ff0bb0ad08646
kimiarm <kimia.rezaei96@gmail.com>
2017-11-05 17:00:45 +0330

    Update past_contests.blade.php

--- resources/views/pages/past_contests.blade.php
+++ resources/views/pages/past_contests.blade.php
@@ -45,6 +45,7 @@
                         </p>
                     </div>
                 </div>--}}
+                <br>
                 2016: (Day 2  - Nov 17th): <a href="http://icpc.aut.ac.ir/files/aut2016.pdf">Problems</a>,  <a href="http://icpc.aut.ac.ir/files/onsite_ranking2016.html">Final Ranklist (Onsite)</a>, <a href="https://www.hackerrank.com/contests/aut-icpc2016-online-version/leaderboard">Final Ranklist (Online)</a>
                 <br>
                 2017: <a href="http://icpc.aut.ac.ir/files/2017-problems.pdf">Problems</a>,  <a href="http://icpc.aut.ac.ir/files/onsite-2017.html">Final On-site Ranklist</a>,  <a href="http://icpc.aut.ac.ir/files/online-2017.html">Final On-line Ranklist</a>
