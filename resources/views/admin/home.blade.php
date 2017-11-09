76d2d3a5a50b955d2447ff9f60df4a8b76900541
kimiarm <kimia.rezaei96@gmail.com>
2017-11-09 21:46:52 +0330

    view updated for generating tsv

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -26,11 +26,23 @@
         </div>
         <div class="row">
             <div class="col s12 m12 l4">
-                <a href="">
+                <a href="{{ route('app::admin.registrations.export.account.tsv') }}">
                     <div class="card cyan darken-2 waves-effect waves-light">
                         <div class="card-content white-text">
-                            <span class="card-title">Finalized on-site teams to tsv</span>
-                            <p>Export all fil=nalized on-site teams to tsv for judge</p>
+                            <span class="card-title">Finalized on-site teams to account.tsv </span>
+                            <p>Export all finalized on-site teams to tsv for judge</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        div class="row">
+            <div class="col s12 m12 l4">
+                <a href="{{ route('app::admin.registrations.export.teams.tsv') }}">
+                    <div class="card cyan darken-2 waves-effect waves-light">
+                        <div class="card-content white-text">
+                            <span class="card-title">Finalized on-site teams to teams.tsv </span>
+                            <p>Export all finalized on-site teams to tsv for judge</p>
                         </div>
                     </div>
                 </a>
