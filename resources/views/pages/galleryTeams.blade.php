a3edaf7759c8b54f269235212787cd4ec88787b3
kimiarm <kimia.rezaei96@gmail.com>
2017-11-12 16:19:26 +0330

    Update galleryTeams.blade.php

--- resources/views/pages/galleryTeams.blade.php
+++ resources/views/pages/galleryTeams.blade.php
@@ -5,6 +5,7 @@
    <a href="http://img.techpowerup.org/171112/dsc-3983.jpg"><img src="http://img.techpowerup.org/171112/dsc-3983.jpg"  height="200" width="300"></a>
     <a href="http://img.techpowerup.org/171112/dsc-3987.jpg"><img src="http://img.techpowerup.org/171112/dsc-3987.jpg"  height="200" width="300"></a>
     <a href="http://img.techpowerup.org/171112/dsc-3992.jpg"><img src="http://img.techpowerup.org/171112/dsc-3992.jpg"  height="200" width="300"></a>
+ <a href="http://img.techpowerup.org/171112/dsc-4000.jpg"><img src="http://img.techpowerup.org/171112/dsc-4000.jpg"  height="200" width="300"></a>
     </div>
   <div class=" col s4 m14 14">
     <a href="http://img.techpowerup.org/171112/dsc-4000.jpg"><img src="http://img.techpowerup.org/171112/dsc-4000.jpg"  height="200" width="300"></a>
