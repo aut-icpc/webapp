3f0f7e2d0efac677e18cd55c5164d3adf0c30341
kimiarm <kimia.rezaei96@gmail.com>
2017-11-12 16:07:14 +0330

    Update galleryTeams.blade.php

--- resources/views/pages/galleryTeams.blade.php
+++ resources/views/pages/galleryTeams.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-   
+   <a href="http://img.techpowerup.org/171112/dsc-3983.jpg"><img src="http://img.techpowerup.org/171112/dsc-3983.jpg" alt="Smiley face" height="200" width="300"></a>
 @endsection
