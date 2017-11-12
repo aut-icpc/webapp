46c8a48b0344b79b8abff6e7de6139a29b0a5458
kimiarm <kimia.rezaei96@gmail.com>
2017-11-12 16:13:42 +0330

    Update galleryTeams.blade.php

--- resources/views/pages/galleryTeams.blade.php
+++ resources/views/pages/galleryTeams.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-   <a href="http://img.techpowerup.org/171112/dsc-3983.jpg"><img src="http://img.techpowerup.org/171112/dsc-3983.jpg" alt="Smiley face" height="200" width="300"></a>
+   <a href="http://img.techpowerup.org/171112/dsc-3983.jpg"><img src="http://img.techpowerup.org/171112/dsc-3983.jpg"  height="200" width="300"></a>
+    <a href="http://img.techpowerup.org/171112/dsc-3987.jpg"><img src="http://img.techpowerup.org/171112/dsc-3987.jpg"  height="200" width="300"></a>
+    <a href="http://img.techpowerup.org/171112/dsc-3992.jpg"><img src="http://img.techpowerup.org/171112/dsc-3992.jpg"  height="200" width="300"></a>
+    <a href="http://img.techpowerup.org/171112/dsc-4000.jpg"><img src="http://img.techpowerup.org/171112/dsc-4000.jpg"  height="200" width="300"></a>
+    <a href="http://img.techpowerup.org/171112/dsc-4004.jpg"><img src="http://img.techpowerup.org/171112/dsc-4004.jpg"  height="200" width="300"></a>
+   <a href="http://img.techpowerup.org/171112/dsc-4014.jpg"><img src="http://img.techpowerup.org/171112/dsc-4014.jpg"  height="200" width="300"></a>
 @endsection
