2c276459a9dec4ba37728c57e24bcc0004e182e5
kimiarm <kimia.rezaei96@gmail.com>
2017-11-21 02:00:41 +0330

    Create gallery.blade.php

---
+++ resources/views/pages/gallery.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="center" style="padding-top: 15%">
+
+    <label >Pre - 2015 </label>
+    <a href="" style="font-size: 20px; color: gray"; >No pictures available!</a>
+    <img id="15" src="{{ asset('img/album15.png') }}" style="height: 100px ; width: 100px">
+    <label for="15">2015</label>
+    <a href="" style="font-size: 20px; color: gray"; >Coming soon!! </a>
+    <img id="16" src="{{ asset('img/album16.png') }}" style="height: 100px ; width: 100px">
+    <label for="16">2016</label>
+    <a href="" style="font-size: 20px; color: gray"; >Coming soon!!</a>
+    <img id="17" src="{{ asset('img/album17.png') }}" style="height: 100px ; width: 100px">
+<label for="17">2017</label>
+
+    <a href="" style="font-size: 20px; color: #0D47A1"; >Teams    </a>
+,
+    <a href="" style="font-size: 20px ; color: #0D47A1">    Others</a>
+
+
+
+
+
+
+</div>
+<br>
+    <br><br>
+<br><br>
+<br><br>
+
+@endsection
+
+
