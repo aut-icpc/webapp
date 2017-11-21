8caad5738c998c0b708ac8a0db0231a986e1ae99
kimiarm <kimia.rezaei96@gmail.com>
2017-11-21 17:31:09 +0330

    add 2014 album

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -5,23 +5,27 @@
 
 <div class="center" style="padding-top: 15%">
 
-    <label >Pre - 2015 </label>
-    <a href="" style="font-size: 20px; color: gray"; >No pictures available!</a>
+    <label >Pre - 2014 </label>
+    <a href="" class="grey lighten-1" >No pictures available!</a>
+    <br>
+     <img id="14" src="{{ asset('img/album14.png') }}" style="height: 100px ; width: 100px">
+    <label for="14">2014</label>
+    <a href="" class="grey lighten-1" >Coming soon!! </a>
     <br>
     <img id="15" src="{{ asset('img/album15.png') }}" style="height: 100px ; width: 100px">
     <label for="15">2015</label>
-    <a href="" style="font-size: 20px; color: gray"; >Coming soon!! </a>
+    <a href="" class="grey lighten-1" >Coming soon!! </a>
     <br>
     <img id="16" src="{{ asset('img/album16.png') }}" style="height: 100px ; width: 100px">
     <label for="16">2016</label>
-    <a href="" style="font-size: 20px; color: gray"; >Coming soon!!</a>
+    <a href="" class="grey lighten-1" >Coming soon!!</a>
     <br>
     <img id="17" src="{{ asset('img/album17.png') }}" style="height: 100px ; width: 100px">
 <label for="17">2017</label>
 
-    <a href="{{route('app::gallery.teams2017')}}" style="font-size: 20px; color: #0D47A1"; >Teams    </a>
+    <a href="{{route('app::gallery.teams2017')}}"  light-blue darken-4 >Teams    </a>
 ,
-    <a href="{{route('app::gallery.others2017')}}" style="font-size: 20px ; color: #0D47A1">    Others</a>
+    <a href="{{route('app::gallery.others2017')}}"  light-blue darken-4>    Others</a>
 
 
 
