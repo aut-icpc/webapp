b62239536709a1c9b0b5f5f936f19f1fda7e13c4
kimiarm <kimia.rezaei96@gmail.com>
2017-11-21 04:03:17 +0330

    Update gallery.blade.php

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -7,12 +7,15 @@
 
     <label >Pre - 2015 </label>
     <a href="" style="font-size: 20px; color: gray"; >No pictures available!</a>
+    <br>
     <img id="15" src="{{ asset('img/album15.png') }}" style="height: 100px ; width: 100px">
     <label for="15">2015</label>
     <a href="" style="font-size: 20px; color: gray"; >Coming soon!! </a>
+    <br>
     <img id="16" src="{{ asset('img/album16.png') }}" style="height: 100px ; width: 100px">
     <label for="16">2016</label>
     <a href="" style="font-size: 20px; color: gray"; >Coming soon!!</a>
+    <br>
     <img id="17" src="{{ asset('img/album17.png') }}" style="height: 100px ; width: 100px">
 <label for="17">2017</label>
 
