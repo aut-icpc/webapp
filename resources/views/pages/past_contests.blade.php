1e410d1fca5c0a7044a99aada59fac2ddca7f161
kimiarm <kimia.rezaei96@gmail.com>
2017-12-10 01:32:39 +0330

    Update day scoreboard link

--- resources/views/pages/past_contests.blade.php
+++ resources/views/pages/past_contests.blade.php
@@ -29,8 +29,9 @@
                 2015: <a href="http://icpc.aut.ac.ir/files/problemset2015.pdf">Problems</a>,  <a href="http://icpc.aut.ac.ir/files/2015ranklist.html">Final Ranklist</a>
                 <br>
                 2016:  (Day 1  - Nov 4th) - (Dismissed):
-                <a href="http://ceit.aut.ac.ir/~sepehr/icpc/index.html">Final Ranklist (Onsite)</a>, 
-                <a href="http://icpc.aut.ac.ir/files/problemset_2016_day1.pdf">Problems</a> {{-- ,   <a href="">More Details </a> --}} ,  Contest dismissed due to technical failures.
+                  <a href="http://icpc.aut.ac.ir/files/problemset_2016_day1.pdf">Problems</a>, 
+            <a href="http://icpc.aut.ac.ir/files/2016_day1.html">Final Ranklist (Onsite)</a> {{-- ,   <a href="">More Details </a> --}} ,  Contest dismissed due to technical failures.
+               
                {{-- <div>
                     <div class="card-content"  id="detail">
                         <p style="font-size: 15px;">
